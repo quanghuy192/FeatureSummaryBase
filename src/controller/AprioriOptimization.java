de352e33af8bbfbc07952558230c3cb6245ad242
huy <quanghuydang1902@gmail.com>
2017-08-13 18:37:01 +0700

    test

---
+++ src/controller/AprioriOptimization.java
@@ -0,0 +1,278 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.I_ComplexArray;
+import model.I_Item;
+import model.Word;
+import utils.GeneralUtil;
+
+/**
+ * Apriori algorithm Find the pattern substring matching in parent string (maybe
+ * optimization with Apriori-TID, Apriori-Habrid algorithm)
+ * 
+ * Optimization
+ * {@link https://www.academia.edu/6823564/The_Optimization_and_Improvement_of_the_Apriori_Algorithm}
+ * 
+ * 1) L1 = {large 1-itemsets}; 2) for (k=2; Lk-1 ≠ ∅ ; k++) 3) { 4)
+ * Ck=apriori-gen(Lk-1); // generate new candidate itemsets 5) for all
+ * transactions t∈D and t.delete=0 6) { 7) if t.count<k then // if the size of
+ * transaction t is less than k, t is useless for Ck generated 8) t.delete=1 //
+ * mark t the deleting tag to skip over the record in next database scanning 9)
+ * else 10) { 11) Ct=subset(Ck,t); // candidate itemsets contained in transaCion
+ * t 12) if Ct = ∅ then // if t does not contain any subset of candidate
+ * itemsets Ck, mark the deleting tag 13) t.delete=1; 14) else 15) { 16) for all
+ * candidates c∈Ct 17) c.count++; 18) } 19) } 20) } 21) Lk={c∈Ck|c.count ≥
+ * minsup} 22) } 23) Answer= kLk;
+ * 
+ * @author dqhuy
+ *
+ */
+public class AprioriOptimization {
+
+	private List<I_ComplexArray> dataOriginalItems;
+	private List<I_ComplexArray> dataResultItems;
+	private int N;
+	private double SUPPORT_MIN = 2;
+
+	public AprioriOptimization() {
+		dataResultItems = new ArrayList<>();
+	}
+
+	public List<I_ComplexArray> generate_K_ItemSet(List<I_ComplexArray> dataItemsParent) {
+
+		GeneralUtil.setTimeStart();
+		dataOriginalItems = dataItemsParent;
+		List<I_ComplexArray> dataResultItemsClone = new ArrayList<>();
+
+		N = dataOriginalItems.size();
+
+		// L1 = {large 1-itemsets};
+		List<I_ComplexArray> candidate = getAtomFirstData(dataItemsParent);
+		dataResultItems = candidate;
+
+		if (dataResultItems.size() > 1) {
+			show(dataResultItems);
+			GeneralUtil.setTimeEnd();
+			System.out.println("Support min = " + SUPPORT_MIN);
+			System.out.println("Count : " + dataResultItems.size() + " items");
+			System.out.println("----------------------------------------");
+			System.out.println("----------------------------------------");
+			System.out.println("----------------------------------------");
+		}
+
+		// for (k=2; Lk-1 ≠ ∅ ; k++)
+		for (int k = 2; candidate.size() > 0; k++) {
+
+			// Ck=apriori-gen(Lk-1);
+			candidate = join(dataResultItems, candidate);// generate new candidate itemsetss
+
+			// for all transactions t∈D and t.delete=0
+			for (I_ComplexArray c : dataOriginalItems) {
+				// for all transactions t∈D and t.delete=0
+				if (!c.isDeleteTag()) {
+					// if t.count<k then // if the size of transaction t is less than k, t is
+					// useless for Ck generated
+					// t.delete=1 //mark t the deleting tag to skip over the record in next database
+					// scanning
+					if (dataOriginalItems.size() < k) {
+						c.setDeleteTag(true);
+					} else {
+						// Ct=subset(Ck,t); //candidate itemsets contained in transaCion t
+						List<I_Item> itemsRule = getSubChild(candidate);
+						showQuantity(itemsRule);
+						int size = itemsRule.size();
+
+						// if Ct = ∅ then // if t does not contain any subset of candidate itemsets Ck,
+						// mark the deleting tag
+						if (size == 0) {
+							c.setDeleteTag(true);
+						} else {
+							// for all candidates c∈Ct
+							// c.count++;
+							itemsRule = GeneralUtil.pruneDuplicateItem(itemsRule);
+							int count = 0;
+
+							dataResultItemsClone = cloneArray(dataResultItems);
+							dataResultItems.clear();
+							// make clone from result data
+							for (I_Item i : itemsRule) {
+								double percent = 1.0 * i.getQuantity();
+								List<Word> subList;
+								if (percent >= SUPPORT_MIN) {
+									subList = i.getItemsChild();
+									I_ComplexArray com = new I_ComplexArray(count, subList);
+									dataResultItems.add(com);
+									count++;
+								}
+							}
+
+							// make clone from result data items if result change
+							if (dataResultItems.size() > 0) {
+								dataResultItemsClone = cloneArray(dataResultItems);
+							}
+						}
+					}
+				}
+			}
+			if (dataResultItems.size() > 1) {
+				show(dataResultItems);
+				GeneralUtil.setTimeEnd();
+				System.out.println("Support min = " + SUPPORT_MIN);
+				System.out.println("Count : " + dataResultItems.size() + " items");
+				System.out.println("----------------------------------------");
+				System.out.println("----------------------------------------");
+				System.out.println("----------------------------------------");
+			}
+		}
+		System.out.println("Count : " + dataResultItemsClone.size() + " items");
+		GeneralUtil.setTimeEnd();
+		return dataResultItemsClone;
+	}
+
+	private void showQuantity(List<I_Item> itemsRule) {
+		System.out.println("======================");
+		System.out.println(itemsRule.size());
+		for (I_Item i : itemsRule) {
+			for (Word w : i.getItemsChild()) {
+				System.out.print(w.getWord() + " ");
+			}
+			System.out.println(" quantity = " + i.getQuantity());
+		}
+
+	}
+
+	public void show(List<I_ComplexArray> result) {
+		System.out.println("RESULT");
+		System.out.println("+++++++++++++++++++++++++++");
+		for (I_ComplexArray s : result) {
+			for (Word i : s.getComplexObject()) {
+				System.out.print(i.getWord() + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private List<I_ComplexArray> cloneArray(List<I_ComplexArray> resultItems) {
+		List<I_ComplexArray> dataResultItemsClone = new ArrayList<>();
+		for (I_ComplexArray c : resultItems) {
+			try {
+				dataResultItemsClone.add(c.clone());
+			} catch (CloneNotSupportedException e) {
+				e.printStackTrace();
+			}
+		}
+		return dataResultItemsClone;
+	}
+
+	private List<I_Item> getSubChild(List<I_ComplexArray> dataItemsParent) {
+		List<I_Item> itemsRule = new ArrayList<>();
+
+		for (I_ComplexArray parent : dataOriginalItems) {
+			for (I_ComplexArray child : dataItemsParent) {
+
+				I_Item i = new I_Item(parent.getComplexObject(), child.getComplexObject());
+				if (!itemsRule.contains(i)) {
+					i.setItemsParent(parent.getComplexObject());
+					itemsRule.add(i);
+				} else {
+					I_Item clone = getItem(child.getComplexObject(), itemsRule);
+					if (null != clone) {
+						clone.setItemsParent(parent.getComplexObject());
+					}
+				}
+			}
+		}
+		return itemsRule;
+	}
+
+	private I_Item getItem(List<Word> child, List<I_Item> items) {
+		for (I_Item i : items) {
+			I_Item temp = new I_Item(child);
+			if (i.equals(temp)) {
+				return i;
+			}
+		}
+		return null;
+	}
+
+	private List<I_ComplexArray> aprioriGen(List<I_ComplexArray> items) {
+		List<I_ComplexArray> dataItemsChild = new ArrayList<>();
+		List<Word> itemAtom = getAtomItems(items);
+
+		System.out.println("items size = " + items.size());
+		System.out.println("item atom size =" + itemAtom.size());
+
+		for (I_ComplexArray s : items) {
+			for (Word a : itemAtom) {
+
+				List<Word> temp = new ArrayList<>();
+				temp.addAll(s.getComplexObject());
+				if (temp.contains(a)) {
+					continue;
+				}
+
+				temp.add(a);
+				I_ComplexArray complex = new I_ComplexArray(temp);
+
+				if (!dataItemsChild.contains(complex)) {
+					dataItemsChild.add(complex);
+				}
+			}
+		}
+		return dataItemsChild;
+	}
+
+	public List<I_ComplexArray> getAtomFirstData(List<I_ComplexArray> dataItemsParent) {
+		List<I_ComplexArray> itemsFirst = new ArrayList<>();
+		List<Word> atomItems = getAtomItems(dataItemsParent);
+		I_ComplexArray com;
+		List<Word> itemString;
+		for (Word s : atomItems) {
+			itemString = new ArrayList<>();
+			itemString.add(s);
+			com = new I_ComplexArray(itemString);
+			itemsFirst.add(com);
+		}
+		return itemsFirst;
+	}
+
+	private List<Word> getAtomItems(List<I_ComplexArray> dataItemsParent) {
+		List<Word> itemList = new ArrayList<>();
+		for (I_ComplexArray com : dataItemsParent) {
+			List<Word> itemValue = com.getComplexObject();
+			for (Word s : itemValue) {
+				if (!itemList.contains(s)) {
+					itemList.add(s);
+				}
+			}
+		}
+		return itemList;
+	}
+
+	private List<I_ComplexArray> join(List<I_ComplexArray> cK1, List<I_ComplexArray> cK2) {
+		List<I_ComplexArray> candidates = new ArrayList<>();
+
+		int size1 = cK1.size();
+		int size2 = cK2.size();
+		List<Word> temp;
+		for (int i = 0; i < size1; i++) {
+			for (int j = i + 1; j < size2; j++) {
+				temp = new ArrayList<>();
+				temp.addAll(cK1.get(i).getComplexObject());
+				List<Word> wordCk2 = cK2.get(j).getComplexObject();
+				for (Word w : wordCk2) {
+					if (!temp.contains(w)) {
+						temp.add(w);
+						break;
+					}
+				}
+				I_ComplexArray complex = new I_ComplexArray(temp);
+				candidates.add(complex);
+			}
+		}
+		return candidates;
+	}
+
+}
--- src/controller/Test.java
+++ src/controller/Test.java
@@ -1,10 +1,12 @@
 package controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
-import model.Feature;
-import utils.FeatureBaseUtils;
+import model.I_ComplexArray;
+import model.Word;
+import utils.WordUtils;
 
 public class Test implements Serializable {
 
@@ -54,37 +56,36 @@ public class Test implements Serializable {
 		// { { "3" }, { "A", "B", "C", "E" } },
 		// { { "4" }, { "B", "E" } } };
 		// // HashMap<Integer, List<String[]>> dataItems = new HashMap<>();
-		// List<I_ComplexArray> dataItems = new ArrayList<>();
-		// List<Word> l1 = new ArrayList<>();
-		// l1.add(new Word("A"));
-		// l1.add(new Word("C"));
-		// l1.add(new Word("D"));
-		// List<Word> l2 = new ArrayList<>();
-		// l2.add(new Word("B"));
-		// l2.add(new Word("C"));
-		// l2.add(new Word("E"));
-		// List<Word> l3 = new ArrayList<>();
-		// l3.add(new Word("A"));
-		// l3.add(new Word("B"));
-		// l3.add(new Word("C"));
-		// l3.add(new Word("E"));
-		// List<Word> l4 = new ArrayList<>();
-		// l4.add(new Word("B"));
-		// l4.add(new Word("E"));
-		//
-		// dataItems.add(new I_ComplexArray(0,l1));
-		// dataItems.add(new I_ComplexArray(1,l2));
-		// dataItems.add(new I_ComplexArray(2,l3));
-		// dataItems.add(new I_ComplexArray(3,l4));
+		List<I_ComplexArray> dataItems = new ArrayList<>();
+		List<Word> l1 = new ArrayList<>();
+		l1.add(new Word("A"));
+		l1.add(new Word("C"));
+		l1.add(new Word("D"));
+		List<Word> l2 = new ArrayList<>();
+		l2.add(new Word("B"));
+		l2.add(new Word("C"));
+		l2.add(new Word("E"));
+		List<Word> l3 = new ArrayList<>();
+		l3.add(new Word("A"));
+		l3.add(new Word("B"));
+		l3.add(new Word("C"));
+		l3.add(new Word("E"));
+		List<Word> l4 = new ArrayList<>();
+		l4.add(new Word("B"));
+		l4.add(new Word("E"));
 
-		// List<I_ComplexArray> result = new
-		// I_AprioriAlgorithrm().generate_K_ItemSet(dataItems);
-		// for (I_ComplexArray s : result) {
-		// for (String i : s.getComplexObject()) {
-		// System.out.print(i + " ");
-		// }
-		// System.out.println();
-		// }
+		dataItems.add(new I_ComplexArray(0, l1));
+		dataItems.add(new I_ComplexArray(1, l2));
+		dataItems.add(new I_ComplexArray(2, l3));
+		dataItems.add(new I_ComplexArray(3, l4));
+
+		List<I_ComplexArray> result = new AprioriOptimization().generate_K_ItemSet(dataItems);
+		for (I_ComplexArray s : result) {
+			for (Word i : s.getComplexObject()) {
+				System.out.print(i.getWord() + " ");
+			}
+			System.out.println();
+		}
 
 		// for (int ii = 0; ii < data.length; ii++) {
 		// String[][] items = data[ii];
@@ -108,29 +109,29 @@ public class Test implements Serializable {
 		// String[] child = {"A","D","G","B"};
 		// System.out.println(al.checkSubArrayContain(parent, child));
 
-		// WordUtils utils = new WordUtils();
-		// List<I_ComplexArray> complexArrays = utils.generateFeatureBase();
-		// I_AprioriAlgorithrm algorithrm = new I_AprioriAlgorithrm();
-		// List<I_ComplexArray> result = algorithrm.generate_K_ItemSet(complexArrays);
-		// // List<I_ComplexArray> result = algorithrm.generate_K_ItemSet(dataItems);
-		// List<I_ComplexArray> items = algorithrm.getAtomFirstData(result);
-		// for (I_ComplexArray s : items) {
-		// for (Word i : s.getComplexObject()) {
-		// System.out.print(i.getWord() + " ");
-		// }
-		// System.out.println();
-		// }
+//		 WordUtils utils = new WordUtils();
+//		 List<I_ComplexArray> complexArrays = utils.generateFeatureBase();
+//		 AprioriOptimization algorithrm = new AprioriOptimization();
+//		 List<I_ComplexArray> result = algorithrm.generate_K_ItemSet(complexArrays);
+//		 // List<I_ComplexArray> result = algorithrm.generate_K_ItemSet(dataItems);
+//		 List<I_ComplexArray> items = algorithrm.getAtomFirstData(result);
+//		 for (I_ComplexArray s : items) {
+//		 for (Word i : s.getComplexObject()) {
+//		 System.out.print(i.getWord() + " ");
+//		 }
+//		 System.out.println();
+//		 }
 
-		FeatureBaseUtils utils = new FeatureBaseUtils();
-		List<Feature> adjectiveList = utils.getEffectiveWords();
-		int count = 0;
+//		FeatureBaseUtils utils = new FeatureBaseUtils();
+//		List<Feature> adjectiveList = utils.getEffectiveWords();
+//		int count = 0;
+//
+//		for (Feature f : adjectiveList) {
+//			count++;
+//			System.out.println(f.getFeature());
+//			System.out.println(f.getOpinionWords() + " ");
+//		}
+//		System.out.println(count);
 
-		for (Feature f : adjectiveList) {
-			count++;
-			System.out.println(f.getFeature());
-			System.out.println(f.getOpinionWords() + " ");
-		}
-		System.out.println(count);
-		
 	}
 }
