17cbc17d59c8ec2e51515dfdca91ffe1c47b24db
Dang Quang Huy <huy_dg@septeni-technology.jp>
2017-09-29 11:37:44 +0700

    update

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -40,9 +40,6 @@ public class Test implements Serializable {
 		bufferedWriter.write("FINAL SUMMARY FOR IPHONE 6S !!! \n\n\n");
 		bufferedWriter.write("Frequent feature \n".toUpperCase());
 
-		int count1, count2, count3;
-		count1 = count2 = count3 = 0;
-
 		List<OpinionResult> list1, list2, list3;
 		list1 = new ArrayList<>();
 		list2 = new ArrayList<>();
@@ -64,17 +61,14 @@ public class Test implements Serializable {
 						if (o.equals(Orientation.POSITIVE)) {
 							list1.add(new OpinionResult(Orientation.POSITIVE, listS.get(i).getOriginalSentences(),
 									featureString));
-							count1++;
 						}
 						if (o.equals(Orientation.NEGATIVE)) {
 							list2.add(new OpinionResult(Orientation.NEGATIVE, listS.get(i).getOriginalSentences(),
 									featureString));
-							count2++;
 						}
 						if (o.equals(Orientation.NEUTRAL)) {
 							list3.add(new OpinionResult(Orientation.NEUTRAL, listS.get(i).getOriginalSentences(),
 									featureString));
-							count3++;
 						}
 					}
 				}
@@ -83,19 +77,19 @@ public class Test implements Serializable {
 			bufferedWriter.write("====================================\n");
 			bufferedWriter.write(com.getComplexObject().get(0).getWord().toUpperCase() + "\n");
 			bufferedWriter.write("\n");
-			bufferedWriter.write("Positive:" + count1 + "\n\n");
+			bufferedWriter.write("Positive:" + list1.size() + "\n\n");
 			for (OpinionResult o1 : list1) {
 				bufferedWriter.write("_ " + o1.toString() + "\n");
 				bufferedWriter.flush();
 			}
 			bufferedWriter.write("\n");
-			bufferedWriter.write("Negative:" + count2 + "\n\n");
+			bufferedWriter.write("Negative:" + list2.size() + "\n\n");
 			for (OpinionResult o2 : list2) {
 				bufferedWriter.write("_ " + o2.toString() + "\n");
 				bufferedWriter.flush();
 			}
 			bufferedWriter.write("\n");
-			bufferedWriter.write("Neutral:" + count3 + "\n\n");
+			bufferedWriter.write("Neutral:" + list3.size() + "\n\n");
 			for (OpinionResult o3 : list3) {
 				bufferedWriter.write("_ " + o3.toString() + "\n");
 				bufferedWriter.flush();
@@ -106,8 +100,6 @@ public class Test implements Serializable {
 		bufferedWriter.write("\n");
 
 		bufferedWriter.write("Infrequent Frequent feature \n".toUpperCase());
-
-		count1 = count2 = count3 = 0;
 
 		for (I_ComplexArray com : inFrequentFeature) {
 			List<Word> listW = com.getComplexObject();
@@ -125,17 +117,14 @@ public class Test implements Serializable {
 						if (o.equals(Orientation.POSITIVE)) {
 							list1.add(new OpinionResult(Orientation.POSITIVE, listS.get(i).getOriginalSentences(),
 									featureString));
-							count1++;
 						}
 						if (o.equals(Orientation.NEGATIVE)) {
 							list2.add(new OpinionResult(Orientation.NEGATIVE, listS.get(i).getOriginalSentences(),
 									featureString));
-							count2++;
 						}
 						if (o.equals(Orientation.NEUTRAL)) {
 							list3.add(new OpinionResult(Orientation.NEUTRAL, listS.get(i).getOriginalSentences(),
 									featureString));
-							count3++;
 						}
 					}
 				}
@@ -144,19 +133,19 @@ public class Test implements Serializable {
 			bufferedWriter.write("====================================\n");
 			bufferedWriter.write(com.getComplexObject().get(0).getWord().toUpperCase() + "\n");
 			bufferedWriter.write("\n");
-			bufferedWriter.write("Positive:" + count1 + "\n\n");
+			bufferedWriter.write("Positive:" + list1.size() + "\n\n");
 			for (OpinionResult o1 : list1) {
 				bufferedWriter.write("_ " + o1.toString() + "\n");
 				bufferedWriter.flush();
 			}
 			bufferedWriter.write("\n");
-			bufferedWriter.write("Negative:" + count2 + "\n\n");
+			bufferedWriter.write("Negative:" + list2.size() + "\n\n");
 			for (OpinionResult o2 : list2) {
 				bufferedWriter.write("_ " + o2.toString() + "\n");
 				bufferedWriter.flush();
 			}
 			bufferedWriter.write("\n");
-			bufferedWriter.write("Neutral:" + count3 + "\n\n");
+			bufferedWriter.write("Neutral:" + list3.size() + "\n\n");
 			for (OpinionResult o3 : list3) {
 				bufferedWriter.write("_ " + o3.toString() + "\n");
 				bufferedWriter.flush();
