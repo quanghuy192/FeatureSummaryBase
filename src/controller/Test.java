8eeac2aee2b2052162fe86fd86e7c890f84d17bf
Dang Quang Huy <huy_dg@septeni-technology.jp>
2017-09-29 11:22:06 +0700

    update

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -12,6 +12,7 @@ import model.I_ComplexArray;
 import model.OpinionResult;
 import model.Review;
 import model.Sentences;
+import model.Word;
 import utils.FeatureBaseUtils;
 import utils.FeatureBaseUtils.Orientation;
 import utils.WordUtils;
@@ -34,11 +35,11 @@ public class Test implements Serializable {
 
 		String FILE_OUTPUT_NAME = "final_summary.txt";
 		FileWriter writer = new FileWriter(new File(FILE_OUTPUT_NAME));
-		@SuppressWarnings("resource")
 		BufferedWriter bufferedWriter = new BufferedWriter(writer);
 
 		bufferedWriter.write("FINAL SUMMARY FOR IPHONE 6S !!! \n\n\n");
-		bufferedWriter.write("Frequent feature");
+		bufferedWriter.write("Frequent feature \n".toUpperCase());
+
 		int count1, count2, count3;
 		count1 = count2 = count3 = 0;
 
@@ -47,89 +48,120 @@ public class Test implements Serializable {
 		list2 = new ArrayList<>();
 		list3 = new ArrayList<>();
 
-		for (Review r : listReview) {
-			List<Sentences> listS = r.getListSentences();
-			for (int i = 0; i < listS.size(); i++) {
-				Orientation o = utilss.getOrientationSentences(listS, i, feature);
-				if (o.equals(Orientation.POSITIVE)) {
-					list1.add(
-							new OpinionResult(Orientation.POSITIVE, listS.get(i).getOriginalSentences().split("/")[0]));
-					count1++;
+		for (I_ComplexArray com : feature) {
+			List<Word> listW = com.getComplexObject();
+			Word w = listW.get(0);
+			String featureString = w.getWord();
+
+			list1.clear();
+			list2.clear();
+			list3.clear();
+			for (Review r : listReview) {
+				List<Sentences> listS = r.getListSentences();
+				for (int i = 0; i < listS.size(); i++) {
+					if (listS.get(i).getOriginalSentences().toLowerCase().contains(featureString)) {
+						Orientation o = utilss.getOrientationSentences(listS, i, feature);
+						if (o.equals(Orientation.POSITIVE)) {
+							list1.add(new OpinionResult(Orientation.POSITIVE, listS.get(i).getOriginalSentences(),
+									featureString));
+							count1++;
+						}
+						if (o.equals(Orientation.NEGATIVE)) {
+							list2.add(new OpinionResult(Orientation.NEGATIVE, listS.get(i).getOriginalSentences(),
+									featureString));
+							count2++;
+						}
+						if (o.equals(Orientation.NEUTRAL)) {
+							list3.add(new OpinionResult(Orientation.NEUTRAL, listS.get(i).getOriginalSentences(),
+									featureString));
+							count3++;
+						}
+					}
 				}
-				if (o.equals(Orientation.NEGATIVE)) {
-					list2.add(
-							new OpinionResult(Orientation.NEGATIVE, listS.get(i).getOriginalSentences().split("/")[0]));
-					count2++;
-				}
-				if (o.equals(Orientation.NEUTRAL)) {
-					list3.add(
-							new OpinionResult(Orientation.NEUTRAL, listS.get(i).getOriginalSentences().split("/")[0]));
-					count3++;
-				}
-				System.out.println(o.name());
+			}
+			bufferedWriter.write("\n");
+			bufferedWriter.write("====================================\n");
+			bufferedWriter.write(com.getComplexObject().get(0).getWord().toUpperCase() + "\n");
+			bufferedWriter.write("\n");
+			bufferedWriter.write("Positive:" + count1 + "\n\n");
+			for (OpinionResult o1 : list1) {
+				bufferedWriter.write("_ " + o1.toString() + "\n");
+				bufferedWriter.flush();
+			}
+			bufferedWriter.write("\n");
+			bufferedWriter.write("Negative:" + count2 + "\n\n");
+			for (OpinionResult o2 : list2) {
+				bufferedWriter.write("_ " + o2.toString() + "\n");
+				bufferedWriter.flush();
+			}
+			bufferedWriter.write("\n");
+			bufferedWriter.write("Neutral:" + count3 + "\n\n");
+			for (OpinionResult o3 : list3) {
+				bufferedWriter.write("_ " + o3.toString() + "\n");
+				bufferedWriter.flush();
 			}
 		}
-		bufferedWriter.write("Positive:" + count1);
-		for (OpinionResult o1 : list1) {
-			bufferedWriter.write(o1.toString());
-			bufferedWriter.flush();
-		}
-		bufferedWriter.write("Negative:" + count2);
-		for (OpinionResult o2 : list2) {
-			bufferedWriter.write(o2.toString());
-			bufferedWriter.flush();
-		}
-		bufferedWriter.write("Neutral:" + count3);
-		for (OpinionResult o3 : list3) {
-			bufferedWriter.write(o3.toString());
-			bufferedWriter.flush();
-		}
-		System.out.println("================================");
+
+		bufferedWriter.write("\n");
+		bufferedWriter.write("\n");
+
+		bufferedWriter.write("Infrequent Frequent feature \n".toUpperCase());
 
 		count1 = count2 = count3 = 0;
-		list1.clear();
-		list2.clear();
-		list3.clear();
 
-		for (Review r : listReview) {
-			List<Sentences> listS = r.getListSentences();
-			for (int i = 0; i < listS.size(); i++) {
-				Orientation o = utilss.getOrientationSentences(listS, i, inFrequentFeature);
-				if (o.equals(Orientation.POSITIVE)) {
-					list1.add(
-							new OpinionResult(Orientation.POSITIVE, listS.get(i).getOriginalSentences().split("/")[0]));
-					count1++;
+		for (I_ComplexArray com : inFrequentFeature) {
+			List<Word> listW = com.getComplexObject();
+			Word w = listW.get(0);
+			String featureString = w.getWord();
+
+			list1.clear();
+			list2.clear();
+			list3.clear();
+			for (Review r : listReview) {
+				List<Sentences> listS = r.getListSentences();
+				for (int i = 0; i < listS.size(); i++) {
+					if (listS.get(i).getOriginalSentences().toLowerCase().contains(featureString)) {
+						Orientation o = utilss.getOrientationSentences(listS, i, inFrequentFeature);
+						if (o.equals(Orientation.POSITIVE)) {
+							list1.add(new OpinionResult(Orientation.POSITIVE, listS.get(i).getOriginalSentences(),
+									featureString));
+							count1++;
+						}
+						if (o.equals(Orientation.NEGATIVE)) {
+							list2.add(new OpinionResult(Orientation.NEGATIVE, listS.get(i).getOriginalSentences(),
+									featureString));
+							count2++;
+						}
+						if (o.equals(Orientation.NEUTRAL)) {
+							list3.add(new OpinionResult(Orientation.NEUTRAL, listS.get(i).getOriginalSentences(),
+									featureString));
+							count3++;
+						}
+					}
 				}
-				if (o.equals(Orientation.NEGATIVE)) {
-					list2.add(
-							new OpinionResult(Orientation.NEGATIVE, listS.get(i).getOriginalSentences().split("/")[0]));
-					count2++;
-				}
-				if (o.equals(Orientation.NEUTRAL)) {
-					list3.add(
-							new OpinionResult(Orientation.NEUTRAL, listS.get(i).getOriginalSentences().split("/")[0]));
-					count3++;
-				}
-				System.out.println(o.name());
+			}
+			bufferedWriter.write("\n");
+			bufferedWriter.write("====================================\n");
+			bufferedWriter.write(com.getComplexObject().get(0).getWord().toUpperCase() + "\n");
+			bufferedWriter.write("\n");
+			bufferedWriter.write("Positive:" + count1 + "\n\n");
+			for (OpinionResult o1 : list1) {
+				bufferedWriter.write("_ " + o1.toString() + "\n");
+				bufferedWriter.flush();
+			}
+			bufferedWriter.write("\n");
+			bufferedWriter.write("Negative:" + count2 + "\n\n");
+			for (OpinionResult o2 : list2) {
+				bufferedWriter.write("_ " + o2.toString() + "\n");
+				bufferedWriter.flush();
+			}
+			bufferedWriter.write("\n");
+			bufferedWriter.write("Neutral:" + count3 + "\n\n");
+			for (OpinionResult o3 : list3) {
+				bufferedWriter.write("_ " + o3.toString() + "\n");
+				bufferedWriter.flush();
 			}
 		}
-		bufferedWriter.write("Positive:" + count1);
-		for (OpinionResult o1 : list1) {
-			bufferedWriter.write(o1.toString());
-			bufferedWriter.flush();
-		}
-		bufferedWriter.write("Negative:" + count2);
-		for (OpinionResult o2 : list2) {
-			bufferedWriter.write(o2.toString());
-			bufferedWriter.flush();
-		}
-		bufferedWriter.write("Neutral:" + count3);
-		for (OpinionResult o3 : list3) {
-			bufferedWriter.write(o3.toString());
-			bufferedWriter.flush();
-		}
-		System.out.println("================================");
-		bufferedWriter.close();
 
 	}
 }
--- src/model/OpinionResult.java
+++ src/model/OpinionResult.java
@@ -4,10 +4,28 @@ import utils.FeatureBaseUtils.Orientation;
 
 public class OpinionResult {
 
-	public OpinionResult(Orientation type, String sentences) {
+	private final String SPLASH_CHAR = "/";
+	private final String BLANK = " ";
+	private String feature;
+
+	public OpinionResult(Orientation type, String sentences, String feature) {
 		super();
 		this.type = type;
 		this.sentences = sentences;
+		this.feature = feature;
+		replaceRedundantChar();
+	}
+
+	private void replaceRedundantChar() {
+		StringBuilder builder = new StringBuilder();
+		String[] arr = sentences.split(BLANK);
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i].contains(SPLASH_CHAR)) {
+				arr[i] = arr[i].split(SPLASH_CHAR)[0];
+			}
+			builder.append(arr[i] + BLANK);
+		}
+		sentences = builder.toString();
 	}
 
 	private Orientation type;
@@ -15,7 +33,23 @@ public class OpinionResult {
 
 	@Override
 	public String toString() {
-		return type.ordinal() + ":" + sentences;
+		return sentences;
+	}
+
+	public Orientation getType() {
+		return type;
+	}
+
+	public void setType(Orientation type) {
+		this.type = type;
+	}
+
+	public String getFeature() {
+		return feature;
+	}
+
+	public void setFeature(String feature) {
+		this.feature = feature;
 	}
 
 }
